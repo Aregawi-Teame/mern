506764f1582d8a2c664a424f53d1c7e145ee77ee
Aregawi-Teame <aregawiyeheys@gmail.com>
2021-09-07 16:52:28 +0300

    Dashboard get Profile

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react';
+import spinner from './glow.gif';
+
+export default () => (
+    <Fragment>
+        <img src={spinner} style ={{ width: '700px', margin: 'auto', display: 'block' }} alt='Loading...' />
+    </Fragment>
+);
